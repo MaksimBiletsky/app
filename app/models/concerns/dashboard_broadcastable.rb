530fb7f3cca715391298136f89415cdb510676fc
just_a_sea_f <maximbiletsky@gmail.com>
2023-11-10 02:39:31 +0200

    Chagnge default log in and sign-up pages

--- app/models/concerns/dashboard_broadcastable.rb
+++ app/models/concerns/dashboard_broadcastable.rb
@@ -8,13 +8,16 @@ module DashboardBroadcastable
   end
 
   def broadcast_to_dashboard
-    User.find_each do |user|
-      broadcast_replace_to("dashboard_#{user.id}", target: "to_do_lists", partial: "dashboard/to_do_lists",
-                                                   locals: {
-                                                     my_to_do_lists: user.to_do_lists.to_a,
-                                                     to_do_lists: ToDoList.where.not(user:).to_a,
-                                                     user:
-                                                   })
-    end
+    User.find_each { |user| broadcast_for_replace_dashboard(user) }
+    broadcast_for_replace_dashboard(nil)
+  end
+
+  def broadcast_for_replace_dashboard(user)
+    broadcast_replace_to(user ? "dashboard_#{user.id}" : "dashboard", target: "to_do_lists", partial: "dashboard/to_do_lists",
+                                                                      locals: {
+                                                                        my_to_do_lists: user ? user.to_do_lists.to_a : [],
+                                                                        to_do_lists: user ? ToDoList.where.not(user:).to_a : ToDoList.all.to_a,
+                                                                        user:
+                                                                      })
   end
 end
